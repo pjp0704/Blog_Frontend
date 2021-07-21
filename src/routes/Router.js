3e01422584f8ea5941ef24cadbd91a354169b23a
pjp0704 <junhyeong25@gmail.com>
2021-07-21 23:38:27 +0900

    <Add> Main Router

---
+++ src/routes/Router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const MainRouter = () => (
+  <>
+    <Header />
+    <h1>Body</h1>
+    <Footer />
+  </>
+);
+
+export default MainRouter;
