abd4dfd51f07ca011d4c6611ffdc0db39f0d6b26
pjp0704 <junhyeong25@gmail.com>
2021-07-21 23:38:41 +0900

    <Add> basic provider and router to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
+import { Provider } from 'react-redux';
+import MainRouter from './routes/Router';
+import store, { history } from './store';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MainRouter />
+      </ConnectedRouter>
+    </Provider>
   );
-}
+};
 
 export default App;
