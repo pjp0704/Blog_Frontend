10670547804fc61919d4ca3824cda9074c4ba5bc
pjp0704 <junhyeong25@gmail.com>
2021-07-22 22:33:11 +0900

    <Add> apply scss custom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import MainRouter from './routes/Router';
 import store, { history } from './store';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/custom.scss';
+
 const App = () => {
   return (
     <Provider store={store}>
