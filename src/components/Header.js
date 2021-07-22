6e010e9df28c7bf374d95512f8132f96c0730f07
pjp0704 <junhyeong25@gmail.com>
2021-07-22 23:06:45 +0900

    <Add> Header with image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Col, Row } from 'reactstrap';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div id="page-header" className="mb-3">
+      <Row>
+        <Col md="6" sm="auto" className="text-center m-auto">
+          <h1>Blog Project</h1>
+          <p>작업 내용을 기록하는 블로그입니다.</p>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Header;
