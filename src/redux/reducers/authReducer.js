a75b1121795bf66ad964b57b693d20520efed94c
pjp0704 <junhyeong25@gmail.com>
2021-07-23 21:57:50 +0900

    <Add> auth reducer

---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,81 @@
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  CLEAR_ERROR_REQUEST,
+  CLEAR_ERROR_SUCCESS,
+  CLEAR_ERROR_FAILURE,
+} from '../types';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  isLoading: false,
+  user: '',
+  userId: '',
+  userName: '',
+  userRole: '',
+  errorMsg: '',
+  successMsg: '',
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        errorMsg: '',
+        isLoading: true,
+      };
+
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+        userId: action.payload.user.id,
+        userRole: action.payload.user.role,
+        errorMsg: '',
+      };
+
+    case LOGIN_FAILURE:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        ...action.payload,
+        token: null,
+        user: null,
+        userId: null,
+        isAuthenticated: false,
+        isLoading: false,
+        userRole: null,
+        errorMsg: action.payload.data.msg,
+      };
+
+    case CLEAR_ERROR_REQUEST:
+      return {
+        ...state,
+      };
+
+    case CLEAR_ERROR_SUCCESS:
+      return {
+        ...state,
+        errorMsg: '',
+        previousMatchMsg: '',
+      };
+
+    case CLEAR_ERROR_FAILURE:
+      return {
+        ...state,
+        errorMsg: 'Clear Error Fail',
+        previousMatchMsg: 'Clear Error Fail',
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
