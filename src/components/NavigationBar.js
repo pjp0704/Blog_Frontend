6343300decd3e98e0ed4eecba16d0b33cd16134c
pjp0704 <junhyeong25@gmail.com>
2021-07-24 18:31:36 +0900

    <Add> toggle open/close with authenticated state

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Navbar, Container, NavbarToggler, Collapse, Nav } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import LoginModal from './modal/LoginModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOGOUT_REQUEST } from '../redux/types';
 
 const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { isAuthenticated, user, userRole } = useSelector(
+    (state) => state.auth
+  );
+  console.log(userRole, 'userRole');
+
+  const dispatch = useDispatch();
+
+  const onLogout = useCallback(() => {
+    dispatch({
+      type: LOGOUT_REQUEST,
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [user]);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <Navbar color="dark" dark expand="lg" className="sticky-top">
@@ -10,13 +35,16 @@ const NavigationBar = () => {
           <Link to="/" className="text-white text-decoration-none">
             Spectre's Personal Blog
           </Link>
-          <NavbarToggler />
-          <Collapse isOpen={true} navbar>
+          <NavbarToggler onClick={handleToggle} />
+          <Collapse isOpen={isOpen} navbar>
             <Nav className="ml-auto d-felx justify-content-around" navbar>
-              {false ? (
+              {isAuthenticated ? (
                 <h1 className="text-white">(authLink)</h1>
               ) : (
-                <h1 className="text-white">(guestLink)</h1>
+                <>
+                  <h1 className="text-white">(guestLink)</h1>
+                  <LoginModal />
+                </>
               )}
             </Nav>
           </Collapse>
