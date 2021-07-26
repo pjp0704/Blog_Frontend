306920848c12e562dafd9c972fb827bfba5df311
pjp0704 <junhyeong25@gmail.com>
2021-07-26 23:17:55 +0900

    <Add> PostCard component to PostCardList.js

--- src/routes/public/PostCardList.js
+++ src/routes/public/PostCardList.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOAD_POST_REQUEST } from '../../redux/types';
+import Helmet from 'react-helmet';
+import { Row } from 'reactstrap';
+import { GrowingSpinner } from '../../components/Spinner';
+import PostCard from '../../components/post/PostCard';
 
 const PostCardList = () => {
-  return <h1>PostCardList</h1>;
+  const { posts } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: LOAD_POST_REQUEST });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Helmet title="Home" />
+      <Row>
+        {posts
+          ? Array.isArray(posts)
+            ? posts.map(({ _id, title, fileUrl, comments, views }) => {
+                return (
+                  <PostCard
+                    id={_id}
+                    title={title}
+                    fileUrl={fileUrl}
+                    comments={comments}
+                    views={views}
+                  />
+                );
+              })
+            : null
+          : GrowingSpinner}
+      </Row>
+    </>
+  );
 };
 
 export default PostCardList;
